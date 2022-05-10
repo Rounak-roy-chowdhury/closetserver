b671a2821b24892b96344d7721fe8b31a0eddf11
Rounak Roy Chowdhury <rounakroychowdhury007@gmail.com>
2022-05-10 20:34:54 +0530

    Update upload_script.sh

--- inspec/upload_script.sh
+++ inspec/upload_script.sh
@@ -2,9 +2,12 @@
 cd /results
 git config --global user.email "rounakroychowdhury007@gmail.com"
 git config --global user.name "Rounak Roy Chowdhury"
+git config --global --add safe.directory /results
+git config --global pull.rebase false
 git init
 git add .
 git commit -m "My first commit"
-git remote add origin https://github.com/Rounak-roy-chowdhury/zap11.git
-#git pull origin rounak-dev
-git push https://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@github.com/Rounak-roy-chowdhury/zap11.git
+git remote add origin https://github.com/Rounak-roy-chowdhury/new.git
+git pull origin master --allow-unrelated-histories
+git add .
+git push https://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@github.com/Rounak-roy-chowdhury/new.git
